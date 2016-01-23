dbb5ab7b6159b0a732d372c1127e11b68b3baeac
Lauren Spiegel <lhspiegel@gmail.com>
2016-01-23 12:29:09 -0800

    Add commenting map

---
+++ commentMapping.js
@@ -0,0 +1,26 @@
+
+var map = {
+    'end of relationship':
+        `Excellent news! He was not nearly good enough for my daughter. I've already set you up on JDate.  ` +
+        `Profile name is HotandReadytoBreed.  Check is out boys http://jdate/HotandReadytoBreed`,
+    'in an open relationship':
+        'Are you trying to kill your mother?  What is Suzie Schwartz going to think?',
+    'soulcycle':
+        `Go for it, honey! I didn't want to mention the few extra pounds from Hannukah. #latkethighs`,
+    'work':
+        `I'm so proud of you for the new very important job!  Any nice gentlmen there? #ticktock #notgettinganyyounger #biologicalclock  #Iwannabeabubbe`,
+    'san francisco':
+        `Don't forget to bring a jacket and an umbrella.  Maybe two umbrellas. #PNEUMONIA`,
+    'restaurant':
+        `Go easy on the dumplings, my little matzo ball.  You know how the carbs make you bloated.  Just like your mother.`,
+    'added a new photo':
+        'Would it kill you to smile for once? #drovehourstotheorthodontist',
+    'yoga':
+        `I see you have time to lie around on the ground with a bunch of strangers like an animal but no time to call your mother.`,
+    'got a tattoo':
+        `You really are trying to kill me.  I'm telling your father. #FEH!`,
+    'Mojave Desert':
+        `Did you bring a sweater?  You know how cold it can get at night!`,
+};
+
+module.exports = map;
