479a90f1c1aeffec45d0b1e566c94746e23748a9
Brian Green <briangreenery@gmail.com>
2016-01-23 12:53:54 -0800

    More work on login flow

--- index.js
+++ index.js
@@ -1,40 +1,53 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
-var fb = require('fb');
 
-function isError(res) {
-  return !res || res.error;
-}
-
-function getError(res) {
-  return res ? res.error : new Error('request failed for unknown reason');
-}
-
-function getUserId(accessToken) {
-  return new Promise(function(resolve, reject) {
-    fb.api('me', {access_token: accessToken}, function(res) {
-      if (isError(res)) {
-        reject(getError(res));
-      } else {
-        resolve(res);
-      }
-    });
-  });
-}
+var db = require('./lib/db')();
+var fb = require('./lib/fb');
 
 app.set('port', (process.env.PORT || 5000));
-
 app.use(express.static(__dirname + '/public'));
 
+app.get('/dumpdb', function(req, res) {
+  res.json(db.dump());
+});
+
 app.post('/mothers', bodyParser.urlencoded({extended: false}), function(req, res) {
-  getUserId(req.body.access_token)
-    .then(function(info) {
-      res.json(info);
+  var accessToken = req.body.access_token;
+  if (accessToken === undefined) {
+    return res.status(400).end('missing access token');
+  }
+  
+  fb.getMe(accessToken)
+    .then(function(user) {
+      db.createMomIfNotExists(user.id, accessToken);
+      res.json({id: user.id, name: user.name});
     })
     .catch(function(err) {
-      console.error(err.toString());
-      res.status(500).end('Facebook API request failed');
+      console.error(err);
+      res.status(400).end('Invalid access token');
+    });
+});
+
+app.post('/child-of/:id', bodyParser.urlencoded({extended: false}), function(req, res) {
+  var accessToken = req.body.access_token;
+  if (accessToken === undefined) {
+    return res.status(400).end('missing access token');
+  }
+  
+  var mom = db.getMom(req.params.id);
+  if (mom === undefined) {
+    return res.status(404).end('bad mom id');
+  }
+  
+  fb.getMe(accessToken)
+    .then(function(user) {
+      db.createChild(user.id, accessToken);
+      db.addChild(mom, user.id);
+    })
+    .catch(function(err) {
+      console.error(err);
+      res.status(500).end('Failed to add child');
     });
 });
 
